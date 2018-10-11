1aa3d35736683136e1611dfb87f8de6f1cee2416
Evan Sinukoff <evan@evan1.local>
2018-10-11 18:13:54 -0400

    removed unused code

--- keystroke/plotting.py
+++ keystroke/plotting.py
@@ -4,10 +4,10 @@ import pandas as pd
 from keystroke import data_prep, features
 from statsmodels.robust.scale import mad as MAD
 from sklearn.manifold import TSNE
-from sklearn.metrics import silhouette_samples, silhouette_score
-
-
+from sklearn.metrics import silhouette_score
+import matplotlib
 import pdb
+
 
 def hist_flight_times(df=None):
 
@@ -18,15 +18,17 @@ def hist_flight_times(df=None):
     groups = df.groupby(['user_num', 'scenario_num']).groups.keys()
 
     grouplist = []
+    pdb.set_trace()
     for i in groups:
-        grouplist.append(df.loc[df.groupby(['user_num', 'scenario_num']).groups[i]])
-
+        grouplist.append(
+            df.loc[df.groupby(['user_num', 'scenario_num']).groups[i]]
+        )
 
     diffs = np.array([])
 
     for dfi in grouplist:
-
         diffs = np.append(diffs, dfi.tstart.diff())
+    pdb.set_trace()
     diffs = diffs[np.isnan(diffs) == False]
     diffs = pd.Series(diffs)
     plt.close('all')
@@ -135,92 +137,13 @@ def hist_keys_per_scenario(df=None):
     plt.savefig('/Users/evan/Code/Insight/plots/hist_keys_per_scenario.pdf', bbox_inches='tight', pad_inches=0.01)
 
 
-def plot_2dhist():
-
-    A = np.zeros([8, 8])
-    A[1-1, 1-1] = -0.37
-    A[1-1, 2-1] = -0.42
-    A[1-1, 3-1] = -0.40
-    A[1-1, 4-1] = -0.50
-    A[1-1, 5-1] = -0.49
-    A[1-1, 6-1] = -0.40
-    A[1-1, 7-1] = -0.44
-    A[1-1, 8-1] = -0.49
-    A[2-1, 1-1] = -0.42
-    A[2-1, 2-1] = -0.42
-    A[2-1, 3-1] = -0.26
-    A[2-1, 4-1] = -0.37
-    A[2-1, 5-1] = -0.38
-    A[2-1, 6-1] = -0.35
-    A[2-1, 7-1] = -0.35
-    A[2-1, 8-1] = -0.33
-    A[3-1, 1-1] = -0.40
-    A[3-1, 2-1] = -0.26
-    A[3-1, 3-1] = -0.33
-    A[3-1, 4-1] = -0.40
-    A[3-1, 5-1] = -0.32
-    A[3-1, 6-1] = -0.22
-    A[3-1, 7-1] = -0.33
-    A[3-1, 8-1] = -0.42
-    A[4-1, 1-1] = -0.50
-    A[4-1, 2-1] = -0.37
-    A[4-1, 3-1] = -0.40
-    A[4-1, 4-1] = -0.53
-    A[4-1, 5-1] = -0.48
-    A[4-1, 6-1] = -0.32
-    A[4-1, 7-1] = -0.42
-    A[4-1, 8-1] = -0.39
-    A[5-1, 1-1] = -0.49
-    A[5-1, 2-1] = -0.38
-    A[5-1, 3-1] = -0.32
-    A[5-1, 4-1] = -0.48
-    A[5-1, 5-1] = -0.48
-    A[5-1, 6-1] = -0.35
-    A[5-1, 7-1] = -0.43
-    A[5-1, 8-1] = -0.40
-    A[6-1, 1-1] = -0.40
-    A[6-1, 2-1] = -0.35
-    A[6-1, 3-1] = -0.22
-    A[6-1, 4-1] = -0.32
-    A[6-1, 5-1] = -0.35
-    A[6-1, 6-1] = -0.41
-    A[6-1, 7-1] = -0.33
-    A[6-1, 8-1] = -0.33
-    A[7-1, 1-1] = -0.44
-    A[7-1, 2-1] = -0.35
-    A[7-1, 3-1] = -0.33
-    A[7-1, 4-1] = -0.42
-    A[7-1, 5-1] = -0.43
-    A[7-1, 6-1] = -0.33
-    A[7-1, 7-1] = -0.43
-    A[7-1, 8-1] = -0.42
-    A[8-1, 8-1] = -0.49
-    A[8-1, 1-1] = -0.49
-    A[8-1, 2-1] = -0.33
-    A[8-1, 3-1] = -0.31
-    A[8-1, 4-1] = -0.39
-    A[8-1, 5-1] = -0.40
-    A[8-1, 6-1] = -0.33
-    A[8-1, 7-1] = -0.42
-    A[8-1, 8-1] = -0.49
-
-    plt.close('all')
-    plt.imshow(A, vmin=-0.5, vmax=-0.2, cmap='binary_r', origin='lower')
-    plt.xticks(np.arange(8), ['1', '2', '3', '4', '5', '6', '7', '8'])
-    plt.yticks(np.arange(8), ['1', '2', '3', '4', '5', '6', '7', '8'])
-    plt.savefig('/Users/evan/Code/Insight/plots/silhouette_distmap.pdf')
-    plt.close('all')
-
-    return
-
-
-
 def plot_tsne(X, y, scenarios):
+    from scipy.stats import rankdata as rd
     #users = df.user_num.values
     plt.close('all')
-    X_embedded = TSNE(learning_rate=20, perplexity=4, n_iter=100000, n_iter_without_progress=1000, init='random').fit(X)
-    print(X_embedded.kl_divergence_)
-    X_embedded = TSNE(learning_rate=20, perplexity=4, n_iter=100000, n_iter_without_progress=1000, init='random').fit_transform(X)
+   # X_embedded = TSNE(learning_rate=20, perplexity=4, n_iter=100000, n_iter_without_progress=1000, init='random').fit(X)
+   # print(X_embedded.kl_divergence_)
+    X_embedded = TSNE(learning_rate=200, perplexity=6, n_iter=100000, n_iter_without_progress=1000, init='random', random_state=25).fit_transform(X)
     print(silhouette_score(X_embedded, y))
     xs = X_embedded[:, 0]
     ys = X_embedded[:, 1]
@@ -232,22 +155,43 @@ def plot_tsne(X, y, scenarios):
     # xs = np.delete(xs, [xmin, xmax, ymin, ymax])
     # ys = np.delete(ys, [xmin, xmax, ymin, ymax])
     # users = np.delete(users, [xmin, xmax, ymin, ymax])
-
-    plt.scatter(xs, ys, c=y, s=y+2, alpha=1)
+    for i in range(int(len(xs)/5)):
+        y[i*5:(i+1)*5] = i
+   # plt.scatter(xs, ys, c=y, s=25, alpha=1, cmap='tab20')
+    plt.scatter(xs, ys, c=y, s=100, cmap='tab20b', alpha=1.0, edgecolor='None')
+    plt.xticks([])
+    plt.yticks([])
+    plt.xlabel('t-SNE Dimension 1', fontsize=13)
+    plt.ylabel('t-SNE Dimension 2', fontsize=13)
+   # plt.scatter(xs, ys, c=y, s=25, alpha=1, cmap='tab20c')
+   # plt.scatter(xs, ys, c=y, s=25, alpha=1, cmap='gist_ncar')
+    cmap = matplotlib.cm.get_cmap('tab20b')
+   # pdb.set_trace()
     for i in range(len(xs)):
-        plt.text(xs[i], ys[i], str(y[i]) + ' ' + str(scenarios[i]))
-
+         marker='${}$'.format(y[i])
+       #  print(y[i]/np.max(y))
+    #    # pdb.set_trace()
+         color = cmap(y[i]/np.max(y))
+         if i < 10:
+             size = 25
+         else:
+             size = 50
+        # print(color)
+         #plt.scatter(xs[i], ys[i], c=color, s=size, marker=marker, alpha=1, cmap='tab20b')
+    #for i in range(len(xs)):
+       #  plt.text(xs[i], ys[i], str(y[i]), color='k', ha='center', va='center', fontsize=10)# + ' ' + str(scenarios[i]))
+   # pdb.set_trace()
     plt.savefig('/Users/evan/Code/Insight/plots/tsne/tsne_test.pdf', bbox_inches='tight', pad_inches=0.01)
     plt.close('all')
 
     return
 
 
-def plot_roc(truneg, trupos):
+def plot_roc(trueneg, truepos):
 
     plt.close('all')
     plt.plot(1.0 - np.array(trueneg), truepos, color='b')
-    plt.xlabel('Fraction Mis-identified as Imposters', fontsize=14)
+    plt.xlabel('False Positives', fontsize=14)
     plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
     plt.yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
     plt.xlim(0, 1)
