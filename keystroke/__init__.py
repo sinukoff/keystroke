8023928d01ebcde03781fa0beac669c718dfb98f
Evan Sinukoff <evan@evan1.local>
2018-10-11 18:11:13 -0400

    removed old data dir as global variable

--- keystroke/__init__.py
+++ keystroke/__init__.py
@@ -1,6 +1,5 @@
 ROOTDIR = '/Users/evan/Code/Insight/keystroke/'
 DATADIR = '/Users/evan/Code/Insight/keystroke/data/'
-#DATADIR = '/Users/evan/Code/Insight/keystroke/data/subset/'
 bins_per_scen = 8
 ngraphs = [1, 2]
 zones = [1, 2, 3, 4, 5, 6, 32, 8]
