3980db28f22bbf0749186af559bfe5c33e0e63c0
Evan Sinukoff <evan@evan1.local>
2018-10-11 17:00:46 -0400

    added global variables

--- keystroke/__init__.py
+++ keystroke/__init__.py
@@ -1,3 +1,7 @@
 ROOTDIR = '/Users/evan/Code/Insight/keystroke/'
 DATADIR = '/Users/evan/Code/Insight/keystroke/data/'
 #DATADIR = '/Users/evan/Code/Insight/keystroke/data/subset/'
+bins_per_scen = 8
+ngraphs = [1, 2]
+zones = [1, 2, 3, 4, 5, 6, 32, 8]
+
